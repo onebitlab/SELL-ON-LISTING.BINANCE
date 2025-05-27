e85033ed7d7c7f98ab650cfb3b7da94089588e83
onebitlab <onebitresearch@gmail.com>
2025-05-27 23:25:30 +0700

    Update

--- config_sample.py
+++ config_sample.py
@@ -1,2 +1,7 @@
-api_key = 'your_binance_api_key'
-api_secret = 'your_binance_api_secret'
+api_key = "your_api_key_here"
+api_secret = "your_api_secret_here"
+pair = "ALTUSDT"
+coins_for_sale = "100"
+price_offset_percent = "1.0"
+order_timeout_seconds = 30            # Timeout before canceling limit order (in seconds)
+pair_check_interval_seconds = 0.5     # Interval to check if pair is listed (in seconds)
